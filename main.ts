3957e71ad543684c9e2b1681f3e0e09400301991
noobdude0 <146701091+noobdude0@users.noreply.github.com>
2023-10-28 18:49:33 +0530

    indescribable changes

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    story.spriteSayText(mySprite2, "ouch", 15, 13, story.TextSpeed.VerySlow)
+    statusbar.value += -10
+    tiles.placeOnRandomTile(otherSprite, sprites.castle.tilePath5)
+})
+function startbattle () {
+    info.setLife(5)
     mySprite = sprites.create(img`
         . . . . . . f f f f . . . . . . 
         . . . . f f f 2 2 f f f . . . . 
@@ -9,17 +15,67 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, ot
         . . f f f f e e e e f f f f . . 
         . f f e f b f 4 4 f b f e f f . 
         . f e e 4 1 f d d f 1 4 e e f . 
-        . . f e e d d d d d d e e f . e 
-        . . . f e e 4 4 4 4 e e f . e e 
-        . . e 4 f 2 2 2 2 2 2 f 4 e e e 
-        . . 4 d f 2 2 2 2 2 2 f d e e . 
-        . . 4 4 f 4 4 5 5 4 4 f e e . . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
         . . . . . f f f f f f . . . . . 
         . . . . . f f . . f f . . . . . 
         `, SpriteKind.Player)
-})
-function startbattle () {
-	
+    mySprite2 = sprites.create(img`
+        ........................
+        ........................
+        ........................
+        ........................
+        ..........ffff..........
+        ........ff1111ff........
+        .......fb111111bf.......
+        .......f11111111f.......
+        ......fd11111111df......
+        ......fd11111111df......
+        ......fddd1111dddf......
+        ......fbdbfddfbdbf......
+        ......fcdcf11fcdcf......
+        .......fb111111bf.......
+        ......fffcdb1bdffff.....
+        ....fc111cbfbfc111cf....
+        ....f1b1b1ffff1b1b1f....
+        ....fbfbffffffbfbfbf....
+        .........ffffff.........
+        ...........fff..........
+        ........................
+        ........................
+        ........................
+        ........................
+        `, SpriteKind.Enemy)
+    statusbar = statusbars.create(50, 5, StatusBarKind.Health)
+    statusbar.attachToSprite(mySprite2)
+    tiles.setCurrentTilemap(tilemap`level1`)
+    mySprite3 = sprites.create(img`
+        . . . . . . . . f f . . . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . . . f f 4 f . . . . . . 
+        . . . . . . f 4 4 4 f . . . . . 
+        . . . . . . f 4 4 4 4 f . . . . 
+        . . . . . f 4 4 4 4 4 f . . . . 
+        . . . . . f 4 4 4 4 4 f . . . . 
+        . . . . . f 4 4 4 4 4 4 f . . . 
+        . . . . f 4 4 4 4 4 4 4 f . . . 
+        . . . . f 4 4 4 4 4 4 4 f . . . 
+        . . . . f 4 4 4 4 4 4 4 f . . . 
+        . . . f 4 4 4 4 4 4 4 4 4 f . . 
+        . . . f 4 4 4 4 4 4 4 4 4 f . . 
+        . . . f 4 4 4 4 4 4 4 4 4 f . . 
+        . . . f 4 4 4 4 4 4 4 4 4 f . . 
+        . . . . f f f f f f f f f . . . 
+        `, SpriteKind.Projectile)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(10, 5))
+    tiles.placeOnTile(mySprite2, tiles.getTileLocation(16, 1))
+    tiles.placeOnRandomTile(mySprite3, sprites.castle.tilePath5)
+    controller.moveSprite(mySprite)
+    scene.cameraFollowSprite(mySprite)
+    mySprite2.follow(mySprite, 70)
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     animation.runImageAnimation(
@@ -128,80 +184,16 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     100,
     false
     )
+    story.spriteSayText(mySprite2, "gotchu", 15, 13, story.TextSpeed.Fast)
     info.changeLifeBy(-1)
     tiles.placeOnTile(mySprite2, tiles.getTileLocation(16, 1))
 })
-let mySprite2: Sprite = null
+let mySprite3: Sprite = null
 let mySprite: Sprite = null
-info.setLife(5)
-mySprite = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-mySprite2 = sprites.create(img`
-    ........................
-    ........................
-    ........................
-    ........................
-    ..........ffff..........
-    ........ff1111ff........
-    .......fb111111bf.......
-    .......f11111111f.......
-    ......fd11111111df......
-    ......fd11111111df......
-    ......fddd1111dddf......
-    ......fbdbfddfbdbf......
-    ......fcdcf11fcdcf......
-    .......fb111111bf.......
-    ......fffcdb1bdffff.....
-    ....fc111cbfbfc111cf....
-    ....f1b1b1ffff1b1b1f....
-    ....fbfbffffffbfbfbf....
-    .........ffffff.........
-    ...........fff..........
-    ........................
-    ........................
-    ........................
-    ........................
-    `, SpriteKind.Enemy)
-let statusbar = statusbars.create(50, 5, StatusBarKind.Health)
-statusbar.attachToSprite(mySprite2)
-tiles.setCurrentTilemap(tilemap`level1`)
-let mySprite3 = sprites.create(img`
-    . . . . . . . . . . . . . f f f 
-    . . . . . . . . . . . . f 9 9 f 
-    . . . . . . . . . . . f 9 6 9 f 
-    . . . . . . . . . . f 9 6 9 f . 
-    . . . . . . . . . f 9 6 9 f . . 
-    . . . . . . . . f 9 6 9 f . . . 
-    . . f f f . . f 9 6 9 f . . . . 
-    . . f 6 f . f 9 6 9 f . . . . . 
-    . . f f 6 f 9 6 9 f . . . . . . 
-    . . . f 6 f 9 9 f . . . . . . . 
-    . . . f f 5 f f . . . . . . . . 
-    . . . 4 e f 6 6 f f . . . . . . 
-    . . 4 e 4 f f f 6 f . . . . . . 
-    f f e 4 . . . f f f . . . . . . 
-    f 5 f . . . . . . . . . . . . . 
-    f f f . . . . . . . . . . . . . 
-    `, SpriteKind.Projectile)
-tiles.placeOnTile(mySprite, tiles.getTileLocation(16, 31))
-tiles.placeOnTile(mySprite2, tiles.getTileLocation(16, 1))
-tiles.placeOnRandomTile(mySprite3, sprites.castle.tilePath5)
-controller.moveSprite(mySprite)
-scene.cameraFollowSprite(mySprite)
-mySprite2.follow(mySprite, 75)
+let statusbar: StatusBarSprite = null
+let mySprite2: Sprite = null
+story.startCutscene(function () {
+    story.printDialog("Boss: Hello mortal, you dare challenge me? You know that people who have tried to defeat me have all failed, and you dare challenge me.", 80, 90, 50, 150)
+    story.printDialog("You: don't care LMAO", 80, 90, 50, 150)
+    startbattle()
+})
